20a8765781de797ac1fed6100de67b3c021847fc
Aleksandras Zagorskas <aleksandras.zagorskas@gmail.com>
2020-12-06 18:29:25 +0200

    stackoverflow spider

--- stack/stack/items.py
+++ stack/stack/items.py
@@ -1,12 +1,6 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy.item import Item, Field
 
 
-class StackItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class StackItem(Item):
+    title = Field()
+    url = Field()
