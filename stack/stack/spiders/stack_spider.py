e2668875734bf0c717e37d6616fa20d432868dad
Aleksandras Zagorskas <aleksandras.zagorskas@gmail.com>
2020-12-06 18:29:35 +0200

    stackoverflow spider

---
+++ stack/stack/spiders/stack_spider.py
@@ -0,0 +1,18 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+
+from stack.stack.items import StackItem
+
+
+class StackSpider(Spider):
+    name = "stack"
+    allowed_domains = ["stackoverflow.com"]
+    start_urls = [
+        "http://stackoverflow.com/questions?pagesize=50&sort=newest",
+    ]
+
+    def parse(self, response):
+        questions = Selector(response).xpath('//div[@class="summary"]/h3')
+
+        for question in questions:
+            item = StackItem()
